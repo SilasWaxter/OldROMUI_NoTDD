bb9e0cb352127d634ac8300ce72067e4e4ec5edd
Silas Waxter <silaswaxter@gmail.com>
2019-07-28 17:13:06 -0700

    Finds devices with VID and PID.  Records their Serial Port.

--- appUSB-Debugger/Form1.cs
+++ appUSB-Debugger/Form1.cs
@@ -26,22 +26,13 @@ namespace appUSB_Debugger
         private uint VID = 0x1A86, PID = 0x7523;
         private USBClassLibrary.USBClass USBPort;
         private System.Collections.Generic.List<USBClassLibrary.USBClass.DeviceProperties> ListOfUSBDeviceProperties;
-        int?[] serial_port_number = { null, null, null};   //? in front of int makes it nullable
 
-        static SerialPort serial_port;
+        public static List<SerialPort> serial_port = new List<SerialPort>();
+        public List<string> com_port_names = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
-
-            serial_port = new SerialPort();
-            serial_port.PortName = "COM6";
-            serial_port.BaudRate = 115200;
-            serial_port.Parity = (Parity)Enum.Parse(typeof(Parity), "none", false);
-            serial_port.DataBits = 8;
-            serial_port.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "1", false);
-            serial_port.Handshake = (Handshake)Enum.Parse(typeof(Handshake), "xon/xoff", false);
-
-            System.Diagnostics.Debug.Write(serial_port);
 
             //USB Connection
             USBPort = new USBClass();
@@ -50,22 +41,56 @@ namespace appUSB_Debugger
             USBPort.USBDeviceRemoved += new USBClass.USBDeviceEventHandler(USBPort_USBDeviceRemoved);
             USBPort.RegisterForDeviceChange(true, this.Handle);
 
-            find_serial_devices();
+            find_serial_ports_with_devices();
+            init_all_serial_ports();
         }
         private void USBPort_USBDeviceAttached(object sender, USBClass.USBDeviceEventArgs e)
         {
-            find_serial_devices();
+            find_serial_ports_with_devices();
         }
 
         private void USBPort_USBDeviceRemoved(object sender, USBClass.USBDeviceEventArgs e)
         {
-            find_serial_devices();
+            find_serial_ports_with_devices();
         }
 
-        void find_serial_devices()
+
+        void init_all_serial_ports()
+        {
+            //creates a generic port with correct communication properties
+            SerialPort port = new SerialPort();
+            port.BaudRate = 115200;
+            port.Parity = (Parity)Enum.Parse(typeof(Parity), "none", true);
+            port.DataBits = 8;
+            port.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "1", false);
+            port.Handshake = (Handshake)Enum.Parse(typeof(Handshake), "XonXoff", true);
+
+            System.Diagnostics.Debug.WriteLine("Serial Ports Init:  ");
+
+            ///<summary>
+            ///for each port named:
+            ///     change the name of the generic port to the named port;
+            ///     add the named port to the serial_port list;
+            ///</summary>
+            for (int i = 0; i < com_port_names.Count; i++)   
+            {
+                port.PortName = com_port_names[i];
+                serial_port.Add(port);
+                System.Diagnostics.Debug.WriteLine(serial_port[i].PortName);
+            }
+        }
+
+        void find_serial_ports_with_devices()
         {
             if (USBClass.GetUSBDevice(VID, PID, ref ListOfUSBDeviceProperties, true))
             {
+                System.Diagnostics.Debug.WriteLine("Found com_ports:  ");
+                foreach (var device in ListOfUSBDeviceProperties)
+                {
+                    com_port_names.Add(device.COMPort);
+
+                    System.Diagnostics.Debug.WriteLine(device.COMPort);
+                }
 
             }
         }
